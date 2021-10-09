ac5de851fcc08d9a5fc4290d885ac9cffb5e484c
potat <potat@DESKTOP-49I0RRH>
2021-10-09 14:23:14 +0900

    CommentValidatorの作成　確認済み

---
+++ src/main/java/models/validators/CommentValidator.java
@@ -0,0 +1,29 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import actions.views.CommentView;
+import constants.MessageConst;
+
+public class CommentValidator {
+    public static List<String> validate(CommentView cv) {
+        List<String> errors = new ArrayList<String>();
+
+      //内容のチェック
+        String contentError = validateContent(cv.getContent());
+        if (!contentError.equals("")) {
+            errors.add(contentError);
+        }
+
+        return errors;
+    }
+    private static String validateContent(String content) {
+        if (content == null || content.equals("")) {
+            return MessageConst.E_NOCONTENT.getMessage();
+        }
+
+        //入力値がある場合は空文字を返却
+        return "";
+    }
+}
